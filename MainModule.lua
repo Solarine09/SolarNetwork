25ca8fe3a2ddc4ef4e0bab512993312ae132b6cc
Solar <75223552+Solarine09@users.noreply.github.com>
2021-02-24 18:59:03 -0500

    Create MainModule.lua

---
+++ MainModule.lua
@@ -0,0 +1,43 @@
+local Network = {}
+local ApplicationModule = {}
+
+coroutine.wrap(function()
+	local Players = game:GetService("Players")
+	local HttpService = game:GetService("HttpService")
+	
+	--[Module]--
+	local function RandomString()
+		local Length = math.random(10, 20)
+		local Array = {}
+
+		for i = 1, Length do
+			Array[i] = string.char(math.random(32, 126))
+		end
+		return table.concat(Array)
+	end	
+	
+	local function printGay()
+		print("solar said ur gay")
+	end
+	
+	--[Table Application]--
+	local Tables = {}
+	
+	local Table = table
+	local CreateTable = table.create
+	local RemoveTable = table.remove
+	local ClearTable = table.clear
+	local PackTable = table.pack
+	local UnpackTable = table.unpack
+	local InsertTable = table.insert
+	local FindTable = table.find
+	
+	--[Math Application]--
+	local Math = {}
+	
+	--[String Application]--
+	local Strings = {}
+	local GenerateString = RandomString()
+	
+	local GeneratePrint = printGay()
+end)()
